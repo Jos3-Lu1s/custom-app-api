ff744afe94e56d7c12461dedea4d8072626b2ec5
JoseLuisTK <5joselbautistac5@gmail.com>
2026-06-25 10:36:14 -0600

    [FIX] custom_app_api: mantener solo el http controller con bearer token

--- __manifest__.py
+++ __manifest__.py
@@ -30,13 +30,13 @@ Características iniciales:
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+        # 'demo/demo.xml',
     ],
     'installable': True,
     'application': True,
